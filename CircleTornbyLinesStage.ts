892c9396c6a7298dfecacbe3414f9be280afa620
Anwesh43 <anweshthecool0@gmail.com>
2019-01-05 22:32:27 +0530

    adding drawCTLNode function

--- CircleTornbyLinesStage.ts
+++ CircleTornbyLinesStage.ts
@@ -5,12 +5,41 @@ const scGap : number = 0.05
 const scDiv : number = 0.51
 const strokeFactor : number = 90
 const sizeFactor : number = 3
+const color : string = "#673AB7"
 
-const maxScale : Function = (scale : number, i : number, n : number) : number => Math,max(0, scale - i / n)
+const maxScale : Function = (scale : number, i : number, n : number) : number => Math.max(0, scale - i / n)
 const divideScale : Function = (scale : number, i : number, n : number) : number => Math.min(1/n, maxScale(scale, i, n)) * n
 const scaleFactor : Function = (scale : number) : number => Math.floor(scale / scDiv)
 const mirrorValue : Function = (scale : number, a : number, b : number) : number => {
     const k = 1 - scaleFactor(scale)
     return (1 - k) / a + k / b
 }
-const updateScale : Function = (scale : number, dir : number, a : number, b : number) => mirroValue(scale, lines, 1) * dir
+const updateScale : Function = (scale : number, dir : number, a : number, b : number) => mirrorValue(scale, lines, 1) * dir
+
+const drawCTLNode : Function = (context : CanvasRenderingContext2D, i : number, scale : number) => {
+    const gap : number = h / (nodes + 1)
+    context.lineWidth = Math.min(w, h) / strokeFactor
+    context.lineCap = 'round'
+    context.strokeStyle = color
+    context.fillStyle =  color
+    const size : number = gap / sizeFactor
+    const rSize : number = size * 0.8
+    const sc1 : number = divideScale(scale, 0, 2)
+    const sc2 : number = divideScale(scale, 1, 2)
+    const xGap : number = size / (lines + 1)
+    context.save()
+    context.translate(w/2, gap * (i + 1))
+    context.rotate(Math.PI / 2 * sc2)
+    context.fillRect(-rSize, -rSize, rSize, rSize)
+    for (var j = 0; j < lines; j++) {
+        const sc : number = divideScale(sc1, j, lines)
+        context.save()
+        context.translate(-size, xGap * (j + 1))
+        context.beginPath()
+        context.moveTo(0, 0)
+        context.lineTo(2 * size * sc, 0)
+        context.stroke()
+        context.restore()
+    }
+    context.restore()
+}

aa10ba4c146eb81f7f2db72b9add78891147e6b9
Anwesh43 <anweshthecool0@gmail.com>
2019-01-05 20:52:29 +0530

    adding scale functions

--- CircleTornbyLinesStage.ts
+++ CircleTornbyLinesStage.ts
@@ -5,4 +5,12 @@ const scGap : number = 0.05
 const scDiv : number = 0.51
 const strokeFactor : number = 90
 const sizeFactor : number = 3
- 
+
+const maxScale : Function = (scale : number, i : number, n : number) : number => Math,max(0, scale - i / n)
+const divideScale : Function = (scale : number, i : number, n : number) : number => Math.min(1/n, maxScale(scale, i, n)) * n
+const scaleFactor : Function = (scale : number) : number => Math.floor(scale / scDiv)
+const mirrorValue : Function = (scale : number, a : number, b : number) : number => {
+    const k = 1 - scaleFactor(scale)
+    return (1 - k) / a + k / b
+}
+const updateScale : Function = (scale : number, dir : number, a : number, b : number) => mirroValue(scale, lines, 1) * dir

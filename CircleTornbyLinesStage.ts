8fa1ae65a9ba15475b48a39926b47c485935e08b
Anwesh43 <anweshthecool0@gmail.com>
2019-01-05 22:36:28 +0530

    adding CircleTornbyLineStage class

--- CircleTornbyLinesStage.ts
+++ CircleTornbyLinesStage.ts
@@ -43,3 +43,33 @@ const drawCTLNode : Function = (context : CanvasRenderingContext2D, i : number, 
     }
     context.restore()
 }
+
+class CircleTornbyLineStage {
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = '#BDBDBD'
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : CircleTornbyLineStage = new CircleTornbyLineStage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}

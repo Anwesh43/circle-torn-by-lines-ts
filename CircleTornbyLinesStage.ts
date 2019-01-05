5d4bb9874c16ce9f30b2092048023ff7b1302c79
Anwesh43 <anweshthecool0@gmail.com>
2019-01-05 19:15:03 +0530

    adding constants

---
+++ CircleTornbyLinesStage.ts
@@ -0,0 +1,8 @@
+const w : number = window.innerWidth, h : number = window.innerHeight
+const nodes : number = 5
+const lines : number = 4
+const scGap : number = 0.05
+const scDiv : number = 0.51
+const strokeFactor : number = 90
+const sizeFactor : number = 3
+ 
